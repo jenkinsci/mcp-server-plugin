5400930b4a601c57bce514800ccdae054e587173
Olivier Lamy <olamy@apache.org>
2025-08-26 20:34:28 +1000

    restore
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/main/java/io/jenkins/plugins/mcp/server/extensions/BuildLogsExtension.java
+++ src/main/java/io/jenkins/plugins/mcp/server/extensions/BuildLogsExtension.java
@@ -46,7 +46,6 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
 import java.util.Optional;
-
 import jenkins.util.SystemProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.input.ReversedLinesFileReader;
