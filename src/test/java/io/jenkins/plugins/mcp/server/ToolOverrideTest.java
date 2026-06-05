121925354ed9e82d306fb329f107619bf5a103f4
Olivier Lamy <olamy@apache.org>
2026-06-05 20:50:26 +1000

    fix copyright
    Signed-off-by: Olivier Lamy <olamy@apache.org>

--- src/test/java/io/jenkins/plugins/mcp/server/ToolOverrideTest.java
+++ src/test/java/io/jenkins/plugins/mcp/server/ToolOverrideTest.java
@@ -2,7 +2,7 @@
  *
  * The MIT License
  *
- * Copyright (c) 2025, Gong Yi.
+ * Copyright (c) 2025, Olivier Lamy.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
